2a15506a6e9ed4345f1dd95a6fcd9cc7d9835b37
Nurlykhan Dastan <nurlykhandasta@gmail.com>
2025-01-18 23:42:41 +0500

    Dice game

---
+++ script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector("[data-container]");
+const actionBtn = document.querySelector("[data-action-btn]");
+const headText = document.querySelector("[data-game-info]");
+
+const generateRandomNumber = () => Math.ceil(Math.random() * 6);
+
+actionBtn.addEventListener("click", () => {
+    render();
+})
+
+const render = () => {
+    const player1 = generateRandomNumber();
+    const player2 = generateRandomNumber();
+    container.innerHTML = "";
+    container.insertAdjacentHTML("afterbegin", `
+        <svg class="dice dice-red">
+            <use href="/Dice_game/sprites.svg#dice-${player1}-icon"></use>
+        </svg>
+        <svg class="dice dice-blue">
+            <use href="/Dice_game/sprites.svg#dice-${player2}-icon"></use>
+        </svg>
+        `)
+    if (player1 === player2) {
+        headText.textContent = "НИЧЬЯ!";
+        headText.style.color = "#000";
+    }
+    else if (player1 > player2) {
+        headText.textContent = "Победил игрок 1";
+        headText.style.color = "#E98B8B";
+    }
+    else {
+        headText.textContent = "Победил игрок 2";
+        headText.style.color = "#449fcc";
+    }
+}
+
+render();
